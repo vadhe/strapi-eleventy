3fa7794dbcb66233e58a3f3e19ce21de39d325d5
Rivaldi Putra <36479850+vadhe@users.noreply.github.com>
2024-05-07 19:29:55 +0800

    Update url strapi blog.js

--- src/_data/blog.js
+++ src/_data/blog.js
@@ -1,6 +1,6 @@
 module.exports = async () => {
     try {
-        const response = await fetch('http://localhost:1337/api/blogs');
+        const response = await fetch('https://authentic-attraction-395c8e1af9.strapiapp.com/api/blogs');
         const data = await response.json();
         console.log(data.data[2]);
         return data.data;
